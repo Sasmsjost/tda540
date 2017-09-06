6f79e31fa66c9a1a55802c813c4afbca81b004d7
Rikard Legge <rikard.legge@gmail.com>
2017-09-06 12:28:10 +0200

    Complete exchange rate and throw calculator

---
+++ src/ExchangeRate.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class ExchangeRate {
+    public static void main(String[] args) {
+        String input;
+
+        input = JOptionPane.showInputDialog("Please specify the exchange rate between EURO and SEK, \n\tEx. Enter '10' for 1 EURO = 10 SEK");
+        double exchangeRate = Double.parseDouble(input);
+
+        input = JOptionPane.showInputDialog("How many SEK would you like to exchange?");
+        double sek = Double.parseDouble(input);
+
+        double euro = sek / exchangeRate;
+
+        String sekFormatted = String.format("%.2f", sek);
+        String euroFormatted = String.format("%.2f", euro);
+
+        JOptionPane.showMessageDialog(null, "Your conversion is complete!\n\n" +
+                "SEK: " + sekFormatted + "\n" +
+                "EURO: " + euroFormatted + "\n\n" +
+                "Rate: " + exchangeRate);
+
+    }
+}
---
+++ src/ThrowCalculator.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class ThrowCalculator {
+
+    public static void main(String[] args) {
+        final double GRAVITY = 9.82;
+
+        String indata = JOptionPane.showInputDialog("What is the velocity? (m/s)");
+        double velocity = Double.parseDouble(indata);
+
+        indata = JOptionPane.showInputDialog("What angle are you firing at? (º)");
+        double angle = Integer.parseInt(indata);
+        double radAngle = Math.toRadians(angle);
+
+        double height = Math.pow(velocity * Math.sin(radAngle), 2) / (2 * GRAVITY);
+        double distance = Math.pow(velocity, 2) * Math.sin(2 * radAngle) / GRAVITY;
+
+        int decimalCount = 2;
+        String formatString = "%." + decimalCount + "f";
+        String formattedHeight = String.format(formatString, height);
+        String formattedDistance = String.format(formatString, distance);
+
+        JOptionPane.showMessageDialog(null, "Your ball was thrown:\n" +
+                "With a maximum height of " + formattedHeight + " meters\n" +
+                "and a distance of " + formattedDistance + " meters");
+
+    }
+}
