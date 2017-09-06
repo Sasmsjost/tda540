f6a6a58f7e8b34e59732d8a44f7e7a1122082de9
kallshem <johan.lenning@telia.com>
2017-09-06 10:32:39 +0200

    inital commit

---
+++ src/ExFel.java
@@ -0,0 +1,12 @@
+public class TExFel {
+    public static void main(String[] arg) {
+        int tal1, tal2;
+        double tal3
+        indata = JOptionPane.showInputDialog("Ange första talet");
+        tal1 = parseInt(indata);
+        indata = JOptionPane.showInputDialog("Ange andra talet);
+        Tal2 = Integer.parseInt(indata);
+        tal3 = tal1 / tal2;
+        JOptionPane.showMessageDialog(null, "Resultatet blev" + tal3);
+    }
+}
