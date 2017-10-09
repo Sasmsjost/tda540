ebf118845f95df2e52dbddf97d7a8cb7e0f1fbc4
Rikard Legge <rikard.legge@gmail.com>
2017-10-09 23:14:01 +0200

    Simplify and remove abstractions
    Probably not as robust as before but works

--- lab2/src/MazeFinder.java
+++ lab2/src/MazeFinder.java
@@ -1,10 +1,5 @@
-//import org.omg.CORBA.PUBLIC_MEMBER;
-
-// The Whole shit could have been solved with a turnRight function.
-
 public class MazeFinder {
     private Robot robot;
-    private Location mazeStart;
 
     public static void main(String[] args) {
         MazeFinder finder = new MazeFinder();
@@ -15,8 +10,8 @@ public class MazeFinder {
     private void createEnviroment() {
         RobotWorld world = RobotWorld.load("src/maze.txt");
         robot = new Robot(1, 2, Robot.EAST, world);
-        robot.setDelay(1);
-        mazeStart = new Location(7, -1);
+        robot.setDelay(10);
+
     }//createEnviroment
 
     // The robot finds the way through a simply connected maze
@@ -24,80 +19,41 @@ public class MazeFinder {
     //        The robot is at the entrance of the maze.
     //after:  The robot is at the exit of the maze
     private void findExit() {
-        while (!atEndOfWorld()) {
+        do  {
             findDirection();
-        }
-
+        } while (!robot.atEndOfWorld());
     }// findExit
 
-    private boolean atEndOfWorld() {
-        return robot.atEndOfWorld() && robot.getLocation() != mazeStart;
-    }
-
+    // Would give nicer abstractions using bitfields, which would separate the navigation and the scanning.
+    // (Using them as a replacement for classes, or structs at least,
+    // which we've assumed are not allowed for at this point in time)
+    // Since using the values (canGoForward,...) of the four directions as input parameters for
+    // another method as four four separate values we deem as being a worse abstraction than the proposed solution.
     private void findDirection() {
-        boolean didSomething =
-                tryGoLeft() ||
-                tryGoForward() ||
-                tryGoRight() ||
-                handleDeadEnd();
+        boolean canGoForward = robot.frontIsClear();
+        robot.turnLeft();
+        boolean canGoLeft = robot.frontIsClear();
+        robot.turnLeft();
+        boolean canTurnAround = robot.frontIsClear();
+        robot.turnLeft();
+        boolean canGoRight = robot.frontIsClear();
+        robot.turnLeft();
 
-        if(!didSomething) {
-            System.out.println("Hm... We are stuck!!!");
+        if(!canGoLeft && canGoForward) {
+            robot.move();
+        } else if (canGoLeft) {
+            robot.turnLeft();
+            robot.move();
+        } else if (canGoRight) {
+            turnRight();
+            robot.move();
+        } else if(canTurnAround){
+            turnAround();
+            robot.move();
+        } else {
+            System.out.println("Shit! We are stuck at the starting position");
             System.exit(1);
         }
-    }
-
-    private boolean tryGoLeft() {
-        robot.turnLeft();
-        if(robot.frontIsClear()) {
-            robot.move();
-            return true;
-        } else {
-            turnRight();
-            return false;
-        }
-    }
-
-    private boolean tryGoForward() {
-        if(robot.frontIsClear()) {
-            robot.move();
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private boolean tryGoRight() {
-        turnRight();
-        if (robot.frontIsClear()) {
-            robot.move();
-            return true;
-        } else {
-            robot.turnLeft();
-            return false;
-        }
-    }
-
-    private boolean handleDeadEnd() {
-        if (isDeadEnd()) {
-            turnAround();
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private boolean isDeadEnd() {
-        boolean frontClear = robot.frontIsClear();
-
-        robot.turnLeft();
-        boolean leftClear = robot.frontIsClear();
-
-        turnAround();
-        boolean rightClear = robot.frontIsClear();
-
-        robot.turnLeft();
-        return !frontClear && ! leftClear && !rightClear;
     }
 
     private void turnRight() {
