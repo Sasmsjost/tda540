2d59684fb1abc25a4e34f01c0ba2d7fb1c345dff
Rikard Legge <rikard.legge@gmail.com>
2017-11-27 23:45:01 +0100

    Fix grass tile rotation

--- lab8/src/towerdefence/graphics/JGrassTile.java
+++ lab8/src/towerdefence/graphics/JGrassTile.java
@@ -6,7 +6,7 @@ import towerdefence.WorldMap;
 public class JGrassTile extends JTile {
     public JGrassTile(World world) {
         super(Texture.get(WorldMap.GRASS), world);
-        float rotation = (float) (Math.round(Math.random() * 4) * Math.PI);
+        float rotation = (float) (Math.round(Math.random() * 4) * Math.PI / 2f);
         setRotation(rotation);
     }
 }
